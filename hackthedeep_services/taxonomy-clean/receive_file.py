3a0774e8148d434b83f2c807cc5e21bd6d2689cb
Annjana Ramesh <annjanar@hotmail.com>
2018-02-10 23:37:27 -0500

    Print rows
    Print rows

--- hackthedeep_services/taxonomy-clean/receive_file.py
+++ hackthedeep_services/taxonomy-clean/receive_file.py
@@ -4,5 +4,7 @@ from pandas import ExcelFile
 
 wb = pd.read_csv('/Volumes/Macintosh HD 2/HACK THE DEEP/Test.csv')
 
-df1 = wb[['Family Name in Database', 'Genus Name', 'Species Name in Database']]
-print(df1)
+dataframe = wb[['Family Name in Database', 'Genus Name', 'Species Name in Database', 'Species Author Name - Last 1 ']]
+
+for row in dataframe.itertuples():
+	print row
