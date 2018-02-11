b14f5b8ea36a506fb7ea8336e55f148c07fa9476
thenu.m <thenu.m@gmail.com>
2018-02-11 00:35:49 -0500

    Add files via upload

--- hackthedeep_services/app/app.py
+++ hackthedeep_services/app/app.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+from dateutil.parser import parse
 from flask import Flask
+
 
 app = Flask(__name__)
 
@@ -14,6 +17,22 @@ def clean():
 def enrich():
 	return "enrich endpoint"
 
+def guess_date(str_date):
+	str_date.replace(" ","")
+	for fmt in ["%Y/%m/%d", "%d-%m-%Y", "%Y%m%d","%B,%Y","%B%d,%Y","%B-%y","%d-%b-%y","%Y","%b-%y","%m/%d/%Y"]:
+		try:
+			return datetime.strptime(str_date, fmt).date()
+		except ValueError:
+			continue
+	return str_date
+
+def format_to_date(str_date):
+	dt=guess_date(str_date)
+	print ("guess_date", dt)
+	if dt is str_date:
+		return "Unresolved"
+	desired_date_format="%Y-%B-%d"
+	return dt.strftime(desired_date_format)
 
 if __name__ == '__main__':
 	app.run(debug=True)
