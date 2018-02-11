c2bb4aa919de70bdf1d6a956b2dda8951b224691
gracexu94 <serihppas@gmail.com>
2018-02-11 11:06:43 -0500

    Basic search on records in appbase

--- gs-map-the-collections-ui/src/pages/Search/components/SearchData.js
+++ gs-map-the-collections-ui/src/pages/Search/components/SearchData.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import { ReactiveBase, CategorySearch } from '@appbaseio/reactivesearch';
+import { ReactiveBase, DataSearch, ResultList } from '@appbaseio/reactivesearch';
 
 export default class SearchData extends Component {
+  onData(res) {
+    return {
+      title: res.Coll_Subset
+    }
+  }
+
   render() {
     return (
       <ReactiveBase
         app="mapTheCollections"
         credentials="yOsE3Mn7d:caa244c4-7dfe-4441-8414-6b5cd6816e07">
-        <CategorySearch
+        <DataSearch
             componentId="searchbox"
-            dataField="Coll_Subset"
-            categoryField="Coll_Subset.raw"
+            dataField={['Coll_Subset', 'Family Name in Database', 'Genus Name', 'Species Name in Database', 'new_country', 'new_continent']}
             placeholder="Search me!"
+            title="Search"
+            highlight={true}
           />
+          <ResultList
+           react={{
+             and: "searchbox"
+           }}
+           onData={this.onData}
+           componentId="Results"
+           dataField="Genus Name"
+           />
       </ReactiveBase>
     );
   }
