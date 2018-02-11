c05355bceeca647e4dcfa9da5d29fa28f218272f
Khulan Myagmardorj <myagmar2@illinois.edu>
2018-02-10 20:16:26 -0500

    drag and drop file uploader on data page

--- gs-map-the-collections-ui/src/pages/Data/index.js
+++ gs-map-the-collections-ui/src/pages/Data/index.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import { Row, Col, Grid } from 'react-bootstrap';
+import Dropzone from 'react-dropzone';
+
+import './Data.css';
 
 class Data extends Component {
+  constructor(props, context) {
+      super(props, context);
+      this.dropFile = this.onDrop.bind(this);
+  }
+
+  onDrop(acceptedFiles, rejectedFiles) {
+    console.log(acceptedFiles);
+
+  }
+
   render() {
     return (
-        <h1>DATA PAGE</h1>
+      <Grid>
+        <Row>
+          <Col xs={12} md={8}>
+            <Dropzone onDrop={this.dropFile}>
+              <div className='data-dropzone-text'>
+                Drag and drop your file or click here to upload
+              </div>
+            </Dropzone>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
