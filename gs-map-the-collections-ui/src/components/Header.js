57df46e2635c48e7c3976a5316e22de96a815567
Rebecca Gutteridge <rebeccagutteridge@Rebeccas-MacBook-Pro.local>
2018-02-11 01:21:05 -0500

    Add Reactive Search App

--- gs-map-the-collections-ui/src/components/Header.js
+++ gs-map-the-collections-ui/src/components/Header.js
@@ -17,6 +17,9 @@ class Header extends Component {
           <NavItem eventKey={2} href="/visualize">
             Visualize
           </NavItem>
+          <NavItem eventKey={3} href="/search">
+            Search
+          </NavItem>
         </Nav>
       </Navbar>
     );
