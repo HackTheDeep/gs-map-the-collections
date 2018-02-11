4a206266f0492fba987abafe45d59277ffe665b9
Annjana Ramesh <annjanar@hotmail.com>
2018-02-10 23:26:32 -0500

    Checking in csv parse
    Checking in csv parse

--- hackthedeep_services/taxonomy-clean/receive_file.py
+++ hackthedeep_services/taxonomy-clean/receive_file.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pandas import ExcelWriter
 from pandas import ExcelFile
 
-wb = pd.read_excel('/Volumes/Macintosh HD 2/HACK THE DEEP/Test.xlsx')
+wb = pd.read_csv('/Volumes/Macintosh HD 2/HACK THE DEEP/Test.csv')
 
 df1 = wb[['Family Name in Database', 'Genus Name', 'Species Name in Database']]
 print(df1)
