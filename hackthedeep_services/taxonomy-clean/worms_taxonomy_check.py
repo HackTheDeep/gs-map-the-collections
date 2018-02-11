c5d2d252a97a18da216478ae2945f92396661529
nietoe <nietoe@umich.edu>
2018-02-11 01:56:17 -0500

    Changes to the taxonomy check
    Changes to the taxonomy check

--- hackthedeep_services/taxonomy-clean/worms_taxonomy_check.py
+++ hackthedeep_services/taxonomy-clean/worms_taxonomy_check.py
@@ -2,11 +2,27 @@
 import requests
 import sys
 
+def make_request_to_worms_multiple(scientificnames):
+	url = "http://www.marinespecies.org/rest/AphiaRecordsByMatchNames"
+
+	params = {'scientificnames[]':scientificnames, 'marine_only':'true'}
+
+	try:
+		r = requests.get(url = url, params = params)
+		r.raise_for_status()
+	except requests.exceptions.HTTPError as err:
+		return {'error': 'Error in request'}
+
+	if (r.status_code == 204):
+		return {'error': 'None Found for species names'}
+
+	data = r.json()
+	return data
+
 def make_request_to_worms(scientificname):
 	url = "http://www.marinespecies.org/rest/AphiaRecordsByMatchNames"
-	names = [scientificname]
 
-	params = {'scientificnames[]':names, 'marine_only':'true'}
+	params = {'scientificnames[]':scientificname, 'marine_only':'true'}
 
 	try:
 		r = requests.get(url = url, params = params)
@@ -43,9 +59,12 @@ def check_taxonomy_name_single(family, genus, species, authority, taxonomy_info)
 
 	return taxonomy_corrected
 
-def get_taxonomy_for_species(family, genus, species, authority):
-	scientificname = genus + ' ' +  species	
+def get_taxonomy_for_single_species(family, genus, species, authority):
+	scientificname = get_scientific_name(genus, species)
 	response = make_request_to_worms(scientificname)
+	return get_taxonomy_for_species(family, genus, species, authority, response)
+
+def get_taxonomy_for_species(family, genus, species, authority, response):
 	if (len(response) == 0):
 		return response
 	if (len(response) == 1 and 'error' in response):
@@ -53,4 +72,28 @@ def get_taxonomy_for_species(family, genus, species, authority):
 	if (len(response) == 1):
 		return check_taxonomy_name_single(family, genus, species, authority, response[0])
 	else:
-		return {'error': 'Obtained more than one result for species name: ' + scientificname}
+		return {'error': 'Obtained more than one result for species name'}
+
+
+def get_scientific_name(genus, species):
+	return genus + ' ' +  species
+
+def handle_taxonomy_list(taxonomy_info, taxonomy_result):
+	i = 0
+	reponses = []
+	for i in range(len(taxonomy_result)):
+		#handle null case
+		response = get_taxonomy_for_species(taxonomy_info[i][0], 
+			taxonomy_info[i][1], taxonomy_info[i][2], taxonomy_info[i][3], taxonomy_result[i])
+		i = i + 1
+		reponses.append(response)
+	return reponses
+
+def get_taxonomy_for_list(taxonomy_info):
+	scientificnames = []
+	#Get all scientific names
+	for x in taxonomy_info:
+		scientificnames.append(get_scientific_name(x[1], x[2]))
+
+	result = make_request_to_worms_multiple(scientificnames)
+	return handle_taxonomy_list(taxonomy_info, result)
