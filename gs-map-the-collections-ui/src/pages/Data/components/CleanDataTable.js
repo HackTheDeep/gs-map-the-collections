6ffc3a33873e769f42bbac27b4150127f63c134d
Khulan Myagmardorj <myagmar2@illinois.edu>
2018-02-11 02:11:22 -0500

    table view for cleaned data

---
+++ gs-map-the-collections-ui/src/pages/Data/components/CleanDataTable.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Table } from 'react-bootstrap';
+
+import CleanDataTableHeaders from './CleanDataTableHeaders';
+import CleanDataTableBody from './CleanDataTableBody';
+
+class CleanDataTable extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    const data = this.props.data;
+    const headers = Object.keys(data[0]);
+
+    var rows = [];
+
+    for(var i = 0; i < data.length; i++) {
+      var keys = Object.keys(data[i]);
+      var row = [];
+
+      for(var j = 0; j < keys.length; j++) {
+        row.push(data[i][keys[j]]);
+      }
+
+      rows.push(row);
+    }
+
+    this.state = {
+      headers: headers,
+      rows: rows
+    };
+  }
+
+  render() {
+    return (
+      <Table responsive>
+        <CleanDataTableHeaders data={this.state.headers}></CleanDataTableHeaders>
+        <CleanDataTableBody data={this.state.rows}></CleanDataTableBody>
+      </Table>
+    );
+  }
+}
+
+export default CleanDataTable;
