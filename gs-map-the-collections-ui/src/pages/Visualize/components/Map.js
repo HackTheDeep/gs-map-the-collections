81d64ea88042b33ce4394a11c562a374c036206f
gracexu94 <serihppas@gmail.com>
2018-02-11 01:36:54 -0500

    Added static markers and basic hover

--- gs-map-the-collections-ui/src/pages/Visualize/components/Map.js
+++ gs-map-the-collections-ui/src/pages/Visualize/components/Map.js
@@ -1,32 +1,42 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const AnyReactComponent = ({ text }) => (
-  <div style={{
-    position: 'relative', color: 'white', background: 'red',
-    height: 40, width: 60, top: -20, left: -30,    
-  }}>
-    {text}
-  </div>
-);
+import Marker from './Marker';
+import { K_SIZE } from './constants';
 
 export default class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markers: [
+        {id: 'A', lat: 40.7813, lng: -73.9740},
+        {id: 'B', lat: 40.7850, lng: -73.9740}
+      ]
+    }
+  }
+
   static defaultProps = {
     center: {lat: 40.7813, lng: -73.9740},
-    zoom: 11
+    zoom: 16
   };
 
   render() {
+    const places = this.state.markers
+      .map(place => {
+        const {id, ...coords} = place;
+        return (
+          <Marker key={id} {...coords} text={id}/>
+        );
+      });
+
     return (
       <GoogleMapReact
+        bootstrapURLKeys={{key:'AIzaSyBfV9I1_FhnzqfmevPiNzhcJ5at2UP3Htc', language: 'en'}}
         defaultCenter={this.props.center}
         defaultZoom={this.props.zoom}
+        hoverDistance={K_SIZE/2}
       >
-        <AnyReactComponent
-          lat={40.7813}
-          lng={-73.9740}
-          text={'Hack the deep!'}
-        />
+        {places}
       </GoogleMapReact>
     );
   }
